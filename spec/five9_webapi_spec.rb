0d1874d1e9222876c55aa4169b655913483c4d5c
Majid <mhadaily@gmail.com>
2016-11-27 20:35:24 +0800

    change version to 0.0.1

--- spec/five9_webapi_spec.rb
+++ spec/five9_webapi_spec.rb
@@ -3,11 +3,8 @@ require 'spec_helper'
 describe Five9Webapi, '#Version' do
   context 'Testing Gem Version' do
     it 'should return correct Version' do
-      expect(Five9Webapi::VERSION).to eq '0.1.0'
+      expect(Five9Webapi::VERSION).to eq '0.0.1'
     end
 
-    it 'should return correct Version' do
-      expect(Five9Webapi::VERSION).to eq '0.2.0'
-    end
   end
 end
